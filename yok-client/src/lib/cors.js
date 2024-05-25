7a1c9f74592f9cc773780888f76a0fe9df9eefaf
Shekhar Sharma <beingperfect76@gmail.com>
2024-05-25 11:02:40 +0530

    update cors

--- yok-client/src/lib/cors.js
+++ yok-client/src/lib/cors.js
@@ -1,6 +1,6 @@
 // lib/cors.js
 export default function corsMiddleware(req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", "*, http://3.110.207.87/");
+	res.setHeader("Access-Control-Allow-Origin", "http://3.110.207.87/");
 
 	res.setHeader(
 		"Access-Control-Allow-Methods",
