b65bb53eeb65be731dcacd23280717fbb470def4
Shekhar Sharma <beingperfect76@gmail.com>
2024-05-25 11:21:28 +0530

    update header

--- yok-client/src/lib/cors.js
+++ yok-client/src/lib/cors.js
@@ -1,15 +1,20 @@
 // lib/cors.js
-export default function corsMiddleware(req, res, next) {
-	res.setHeader("Access-Control-Allow-Origin", "http://3.110.207.87/");
 
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"GET, POST, PUT, DELETE, OPTIONS"
-	);
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"Content-Type, Authorization"
-	);
+export default function corsMiddleware(req, res, next) {
+	const allowedOrigin = "http://3.110.207.87";
+
+	// Check if the header is already set
+	if (!res.headersSent) {
+		res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+		res.setHeader(
+			"Access-Control-Allow-Methods",
+			"GET, POST, PUT, DELETE, OPTIONS"
+		);
+		res.setHeader(
+			"Access-Control-Allow-Headers",
+			"Content-Type, Authorization"
+		);
+	}
 
 	if (req.method === "OPTIONS") {
 		res.status(200).end();
--- yok-client/src/pages/api/route.js
+++ yok-client/src/pages/api/route.js
@@ -1,12 +1,12 @@
-export const dynamic = 'force-dynamic' // defaults to auto
+export const dynamic = "force-dynamic"; // defaults to auto
 
 export async function GET(request) {
-    return new Response('Hello, Next.js!', {
-        status: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        },
-    })
+	return new Response("Hello, Next.js!", {
+		status: 200,
+		headers: {
+			"Access-Control-Allow-Origin": "http://3.110.207.87",
+			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+			"Access-Control-Allow-Headers": "Content-Type, Authorization",
+		},
+	});
 }
